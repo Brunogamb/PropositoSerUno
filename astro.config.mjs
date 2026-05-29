9cf925365205ea4b5283989a24ead53683c799dd
Brunogamb <brunogamberini24@gmail.com>
2026-05-28 22:51:39 -0300

    fix: update site URL in configuration, enhance JSON-LD structure, and add robots.txt and sitemap.xml for improved SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/Brunogamb/',
+  site: 'https://brunogamb.github.io/PropositoSerUno',
   base: '/PropositoSerUno/',
   vite: {
     build: {
